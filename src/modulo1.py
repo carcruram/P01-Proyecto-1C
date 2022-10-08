74fc1adf3ac6710d46fe12574a32f63d20b6c384
Carlos Javier Cruz Ramírez <carcruram@alum.us.es>
2022-10-08 13:26:53 +0200

    cambio

--- src/modulo1.py
+++ src/modulo1.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 import csv
 from datetime import datetime
+
 #Función para leer formato fecha y hora
 def parse_date(cadena, formato = "%d/%m/%Y"):
     return datetime.strptime(cadena, formato).date()
@@ -10,6 +11,7 @@ def parse_time(cadena, formato = '%H:%M:%S'):
 
 def parse_datetime(cadena, formato = '%d/%m/%Y-%H:%M:%S'):
     return datetime.strptime(cadena, formato)
+
 #Función para leer formato booleano
 def parse_bool(cadena):
     if cadena == 'CSK':
@@ -18,21 +20,21 @@ def parse_bool(cadena):
         booleano = False
     return booleano
 
-cricket = namedtuple("cricket", "Name, Team, Nationality, Matches, Bat_Runs, Bat_Average, Catches")
-
+cricket = namedtuple("cricket", "Name, Team, Nationality, Matches, Bat_Runs, Bat_Average, Catches, Debut")
+#Función para leer los datos del csv
 def lee_csv(dataset1):
     datos = []    
     with open(dataset1, encoding='utf-8') as f:
         lector = csv.reader(f, delimiter = ';')
         next(lector)
-        for Name, Team, Nationality, Matches, Bat_Runs, Bat_Average, Catches in lector:
+        for Name, Team, Nationality, Matches, Bat_Runs, Bat_Average, Catches, Debut in lector:
             Team = parse_bool(Team)
             Matches = int(Matches)
             Bat_Runs = int(Bat_Runs)
             Bat_Average = float(Bat_Average)
             Catches = int(Catches)
-            tupla = cricket(Name, Team, Nationality, Matches, Bat_Runs, Bat_Average, Catches)
+            Debut = parse_datetime(Debut)
+            tupla = cricket(Name, Team, Nationality, Matches, Bat_Runs, Bat_Average, Catches, Debut)
             datos.append(tupla)
         return(datos)
 datos = lee_csv("./data/dataset1.csv")
-print(datos[:3])
--- src/modulo1_test.py
+++ src/modulo1_test.py
@@ -1,2 +1,6 @@
+from modulo1 import lee_csv
+
+datos = lee_csv("./data/dataset1.csv")
+
 if __name__ == '__main__':
-    
+    print(datos[:23])
